9a8ef970d82b3eac9ac505f7b2f21ab8ccdcf6f3
darrenbruegman <27bruegmand@mce-r5.com>
2025-01-31 12:23:33 -0600

    Update index.js

--- games/blackjack/index.js
+++ games/blackjack/index.js
@@ -1,8 +1,9 @@
 let sum = 0;
 let dealersum = 0;
+let topmsg = document.getElementById("msg");
 let playercards = document.getElementById("sum");
 let dealercards = document.getElementById("dealer");
-
+let xrc;
 function randint(min, max) { // min and max included 
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -13,6 +14,7 @@ function newGame() {
   dealersum = 0
   sum += randint(1, 11)
   sum += randint(1, 11)
+  topmsg.textContent = "Blackjack";
   playercards.textContent = "You: " + sum;
   dealercards.textContent = "Dealer: " + dealersum;
 }
@@ -24,17 +26,28 @@ function hit() {
     newGame()
   }
 }
-function stand() {
-  dealersum += randint(1,11)
-  dealersum += randint(1,11)
-  dealercards.textContent = "Dealer: " + dealersum
-   let xrc = setInterval(function(){
+function dealerhit() {
     if (dealersum <= 18) {
       dealersum += randint(1,11)
       dealercards.textContent = "Dealer: " + dealersum
     }
-    else {
-      
-    }
-}, 2000);
+}
+function stand() {
+  dealersum += randint(1,11)
+  dealersum += randint(1,11)
+  dealercards.textContent = "Dealer: " + dealersum
+  xrc = setInterval(dealerhit(), 1500);
+  clearInterval(xrc)
+  if (dealersum == 21) {
+    topmsg.textContent = "The Dealer Has Blackjack!"
+  }
+  else if (dealersum >= sum && dealersum <= 21) {
+    topmsg.textContent = "The Dealer Wins!"
+  }
+  else if (sum == 21 && dealersum != 21) {
+    topmsg.textContent = "You Have Blackjack!"
+  }
+  else if (sum >= dealersum && sum <= 21) {
+    topmsg.textContent = "You Are Win!!!!!"
+  }
 }
