c208a3839aa347a46e47b344762ac2faa1011c63
darrenbruegman <27bruegmand@mce-r5.com>
2025-01-31 11:58:57 -0600

    Update index.js

--- games/blackjack/index.js
+++ games/blackjack/index.js
@@ -9,7 +9,32 @@ function randint(min, max) { // min and max included
 
 
 function newGame() {
+  sum = 0
+  dealersum = 0
   sum += randint(1, 11)
   sum += randint(1, 11)
   playercards.textContent = "You: " + sum;
+  dealercards.textContent = "Dealer: " + dealersum;
+}
+function hit() {
+  sum += randint(1, 11)
+  playercards.textContent = "You: " + sum;
+  if (sum >= 21) {
+    window.alert("you bust!\nStarting New Game!")
+    newGame()
+  }
+}
+function stand() {
+  dealersum += randint(1,11)
+  dealersum += randint(1,11)
+  dealercards.textContent = "Dealer: " + dealersum
+   let xrc = setInterval(function(){
+    if (dealersum <= 18) {
+      dealersum += randint(1,11)
+      dealercards.textContent = "Dealer: " + dealersum
+    }
+    else {
+      
+    }
+}, 2000);
 }
