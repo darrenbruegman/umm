58d943b20750a4a538aedd17196311b4c14ffd8c
darrenbruegman <27bruegmand@mce-r5.com>
2025-01-31 09:57:41 -0600

    Update index.js

--- games/blackjack/index.js
+++ games/blackjack/index.js
@@ -1,2 +1,17 @@
-let playercards = document.getElementById("")
-let dealercardsa = document.getElementById("")
+let sum = 0;
+let dealersum = 0;
+let playercards = document.getElementById("sum")
+let dealercards = document.getElementById("dealer")
+
+function randint(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+function newGame() {
+  sum =+ randint(1, 10)
+  sum =+ randint(1, 10)
+  playercards.textContent = "Sum: " + sum;
+}
+function hit() {}
+function stand() {}
